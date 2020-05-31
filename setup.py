f7e15d02c1fe96c1b944e49055d8866d297dae77
Laetitia Berthelot <leiaz@mailbox.org>
2020-05-31 18:52:44 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='awoxmeshlight',
-        version='0.1',
+        version='0.1.0',
         description='A package to control Awox Mesh light bulbs.',
+        long_description = long_description,
+        long_description_content_type="text/markdown",
         url='http://github.com/leiaz/python-awox-mesh-light',
         author='Laetitia Berthelot',
-        author_email='leiaz@free.fr',
+        author_email='leiaz@mailbox.org',
         license='MIT',
         packages=['awoxmeshlight'],
         classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7'
         ],
         install_requires=['bluepy', 'pycrypto']
         )
